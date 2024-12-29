d7181414eca16d7a26ecca725ac9e0ec1223ee22
EliasPereirah <eliaspereirah@localhost>
2024-12-29 15:03:10 -0300

    Update

--- index.php
+++ index.php
@@ -1,6 +1,9 @@
 <?php
 header("Content-Type: application/json");
-header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Origin: *"); // This allows all origins, to only allow requests from a specific
+// domain remove this line and change the commented line below.
+/// header("Access-Control-Allow-Origin: https://www.example.com");
+
 header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
 if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
     http_response_code(200);
